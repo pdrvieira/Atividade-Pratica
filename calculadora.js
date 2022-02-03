2ded6924bc2e5ee6b013675abf4a6f5456b50877
pdrvieira <pedroavcleite@gmail.com>
2022-02-03 19:43:57 -0300

    perf: adicionando eval() na calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,31 +1,11 @@
-const soma = () => {
-    console.log(parseInt(args[1]) + parseInt(args[2]));
-};
-
-const sub = () => {
-    console.log(parseInt(args[1]) - parseInt(args[2]));  
-}
-
-const div = () => {
-    console.log(parseInt(args[1]) / parseInt(args[2]));  
-}
-
-
 const args = process.argv.slice(2);
 
-switch (args[0]) {
-    case 'soma':
-        soma();
-    break;
+var x = args[0];
+var y = args[2];
+var operator = args[1];
 
-    case 'sub':
-        sub();
-    break;
-
-    case 'div':
-        div();
-    break;
-
-    default:
-        console.log('does not support', args[0]);
+function evaluate(param1, param2, operator) {
+  return eval(param1 + operator + param2);
 }
+
+if ( console.log( evaluate(x, y, operator) ) ) {}
